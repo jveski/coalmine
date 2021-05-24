b32017cade1cb25c4291c1afb4bd89c994613f09
Jordan Olshevski <jordan.olshevski@microsoft.com>
2021-05-24 14:25:34 -0500

    Merge context.go into feature.go and rename

--- coalmine.go
+++ coalmine.go
@@ -131,6 +131,51 @@ func WithPercentage(key Key, percent uint32) MatcherOption {
 	}
 }
 
+type featureOverrideKey string
+
+// WithFeatureOverride forces the given feature to be either enabled or disabled. Useful in tests.
+func WithFeatureOverride(ctx context.Context, feature *Feature, enable bool) context.Context {
+	return context.WithValue(ctx, featureOverrideKey(feature.name), enable)
+}
+
+func getFeatureOverride(ctx context.Context, feature string) (bool /* state */, bool /* present */) {
+	val := ctx.Value(featureOverrideKey(feature))
+	if val == nil {
+		return false, false
+	}
+	return val.(bool), true
+}
+
+type globalOverrideKey struct{}
+
+// WithGlobalOverride forces all features to be either enabled or disabled. Useful in tests.
+func WithGlobalOverride(ctx context.Context, enable bool) context.Context {
+	return context.WithValue(ctx, globalOverrideKey{}, enable)
+}
+
+func getGlobalOverride(ctx context.Context) (bool, bool) {
+	val := ctx.Value(globalOverrideKey{})
+	if val == nil {
+		return false, false
+	}
+	return val.(bool), true
+}
+
+type valueKey string
+
+// WithValue adds a string kv pair to the context for use with matchers.
+func WithValue(ctx context.Context, key Key, value string) context.Context {
+	return context.WithValue(ctx, valueKey(key), value)
+}
+
+func getValue(ctx context.Context, key Key) string {
+	val := ctx.Value(valueKey(key))
+	if val == nil {
+		return ""
+	}
+	return val.(string)
+}
+
 func checkKillswitch(name string) bool {
 	for _, feat := range strings.Split(os.Getenv("COALMINE_KILLSWITCH"), ",") {
 		if feat == name {
